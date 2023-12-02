9ce9b61e43221aade7f6003f628cf728e4b9e0fe
Sean Sanchez <152622040+driver-bail@users.noreply.github.com>
2023-12-02 18:04:29 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// Prompt the user for their name
+var userName = prompt("What's your name?");
+
+// Display a greeting
+if (userName) {
+    alert("Hello, " + userName + "! Welcome to the Greeting App.");
+} else {
+    alert("Hello, Guest! Welcome to the Greeting App.");
+}
